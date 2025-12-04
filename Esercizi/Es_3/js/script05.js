6d2f335e99592aac1ddb3d9bddf430991a1fec6d
Dario <dario.mennillo@immaginazioneelavoro.it>
2025-12-04 17:04:34 +0100

    update lezione 04/12

--- Esercizi/Es_3/js/script05.js
+++ Esercizi/Es_3/js/script05.js
@@ -1,48 +1,6 @@
-let btn = document.querySelector("#btn");
+let stringa = document.getElementById("ruotaAnim").textContent;
 
-//VERS 1
-// let contatore = 0;
-
-// function stampaUnoAllaVolta(){
-//     let parole = ["ciao", "come", "stai", "tutto", "bene"]
-//     console.log(parole[contatore]);
-//     contatore++;
-// }
-
-// //Att, la funzione va collegata senza le parentesi tonde()
-// btn.addEventListener("click", stampaUnoAllaVolta);
-
-//VERS2
-// let contatore = 0;
-// /**
-//  * 
-//  * @param {String[]} parole 
-//  */
-// function stampaUnoAllaVolta(parole){
-//     console.log(parole[contatore]);
-//     contatore++;
-// }
-
-// btn.addEventListener("click", function(){
-//     let parole = ["ciao", "come", "stai", "tutto", "bene"];
-//     stampaUnoAllaVolta(parole);
-// })
-
-let  contatore = 0;
-
-function stampaParola(parola){
-    if(parola != undefined){
-        console.log(parola);
-        contatore++;
-    }else{
-        console.log("So finiteeee");
-    }
-}
-
-function unaAllaVolta(){
-    let parole = ["ciao", "come", "stai", "tutto", "bene"];
-
-    stampaParola(parole[contatore]);
-}
-
-btn.addEventListener("click", unaAllaVolta);
+setInterval(()=>{
+    stringa = stringa[stringa.length - 1] + stringa.slice(0, -1);
+    document.getElementById("ruotaAnim").textContent = stringa
+}, 100)
